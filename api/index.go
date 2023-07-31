b9611cf2a0e66e6557e2af88fb6b59e2e28771cd
bhargav-robosoft <119998070+bhargav-robosoft@users.noreply.github.com>
2023-07-31 17:16:29 +0530

    Added routes endpoints for vercel

--- api/index.go
+++ api/index.go
@@ -31,5 +31,45 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 	})
 
+	server.GET("/get-recents", func(ctx *gin.Context) {
+		data, err := weatherController.GetRecentsWeather(ctx)
+		if err != nil {
+			errorData := entity.WeatherError{
+				Status:  404,
+				Message: err.Error(),
+			}
+			ctx.JSON(404, errorData)
+		} else {
+			ctx.JSON(200, data)
+		}
+	})
+
+	server.GET("/get-favourites", func(ctx *gin.Context) {
+		data, err := weatherController.GetFavouritesWeather(ctx)
+		if err != nil {
+			errorData := entity.WeatherError{
+				Status:  404,
+				Message: err.Error(),
+			}
+			ctx.JSON(404, errorData)
+		} else {
+			ctx.JSON(200, data)
+		}
+	})
+
+	server.GET("/handle-favourite", func(ctx *gin.Context) {
+		response, err := weatherController.HandleFavourite(ctx)
+		if err != nil {
+			errorData := entity.WeatherError{
+				Status:  404,
+				Message: err.Error(),
+				Example: "/get-weather?location=udupi",
+			}
+			ctx.JSON(404, errorData)
+		} else {
+			ctx.JSON(200, response)
+		}
+	})
+
 	server.ServeHTTP(w, r)
 }
